782fb1d6c83562b20c354d198d5db59804f14688
cw <cw@DESKTOP-OO43H4B>
2022-09-26 20:58:23 +0530

    Average of odd digits of Array

--- src/com/project1/ArrayOdd.java
+++ src/com/project1/ArrayOdd.java
@@ -3,21 +3,29 @@ package com.project1;
 import java.util.Scanner;
 
 public class ArrayOdd {
-
+//incomplete code
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		int a[] = new int[5];
 		System.out.println("enter elements in int value");
 		Scanner sc = new Scanner(System.in);
 		for (int i = 0; i < a.length; i++) {
 			a[i] = sc.nextInt();
 		}
+		int sum = 0;
+		int count=0;
+		int avg;
 		for (int i = 0; i < a.length; i++) {
-			//System.out.println(a[i]);
-			if(a[i]%2!=0) {
-				System.out.println(a[i]);
+			if (a[i] % 2 != 0) {
+				count++;
+				sum = a[i] + sum;
 			}
+
 		}
+		avg = sum / count;
+		System.out.println("avererage of odd digits is = " +avg);
+		System.out.println("count of odd digits is = "+count);
 	}
 
 }
